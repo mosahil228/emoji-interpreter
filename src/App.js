723f666c223c5500c42fe69825058c5917c973eb
mosahil228 <54403198+mosahil228@users.noreply.github.com>
2022-07-06 21:12:50 +0530

    more emoji added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import todo from "./todo.png"
 import PulseLoader from "react-spinners/PulseLoader";
 const App = () => {
@@ -6,31 +6,41 @@ const App = () => {
   const [input, setInput] = useState("")
   const [menu, setMenu] = useState(true);
   const [btnName, setBtnName] = useState("Show More")
-  const [len, setLen] = useState(43);
+  const [len, setLen] = useState(50);
   const [loading, setLoading] = useState(false);
 
 
   var emojiDictionary = {
-    "😊": "smiling",
-    "🙄": "disbelief",
-    "😞": "sad",
-    "🥡": "takeout box",
-    "❤️": "love",
-    "😡": "annoyance",
     "😃": "Grinning Face with Big Eyes",
+    "😀": "Grinning Face",
+    "😄": "Grinning Face with Smiling Eyes",
+    "😁": "Beaming Face with Smiling Eyes",
+    "😆": "Grinning Squinting Face",
+    "😅": "Grinning Face with Sweat",
     "🤣": "Rolling on the Floor Laughing",
     "😂": "Face with Tears of Joy",
+    "🙂": "Slightly Smiling Face",
+    "🙃": "Upside-Down Face",
     "😉": "Winking Face",
+    "😊": "Smiling Face with Smiling Eyes",
+    "😇": "Smiling Face with Halo",
     "🥰": "Smiling Face with Hearts",
+    "😍": "Smiling Face with Heart-Eyes",
     "🤩": "Star-Struck",
-    "😋": "Face Savoring Food",
-    "😛": "Face with Tongue",
-    "😜": "Winking Face with Tongue",
-    "🤑": "Money-Mouth Face",
-    "🤭": "Face with Hand Over Mouth",
-    "🤫": "Shushing Face",
-    "🤔": "Thinking Face",
-    "😐": "Neutral Face",
+    "😘": "Face Blowing a Kiss",
+    "😗": "Kissing Face",
+    "💀": "Skull",
+    "☠️": "Skull and Crossbones",
+    "💩": "Pile of Poo",
+    "🤡": "Clown Face",
+    "👹": "Ogre",
+    "👺": "Goblin",
+    "👻": "Ghost",
+    "👽": "Alien",
+    "👾": "Alien Monster",
+    "🤖": "Robot",
+    "🥡": "takeout box",
+    "❤️": "love",
     "🙊": "Speak-No-Evil Monkey",
     "💥": "Collision",
     "💫": "Dizzy",
@@ -41,6 +51,83 @@ const App = () => {
     "🦍": "Gorilla",
     "🦧": "Orangutan",
     "🐶": "Dog Face",
+    "😚": "Kissing Face with Closed Eyes",
+    "😙": "Kissing Face with Smiling Eyes",
+    "🥲": "Smiling Face with Tear",
+    "😋": "Face Savoring Food",
+    "😛": "Face with Tongue",
+    "😜": "Winking Face with Tongue",
+    "🤪": "Zany Face",
+    "😝": "Squinting Face with Tongue",
+    "🤑": "Money-Mouth Face",
+    "🤗": "Smiling Face with Open Hands",
+    "🤭": "Face with Hand Over Mouth",
+    "🤫": "Shushing Face",
+    "🤔": "Thinking Face",
+    "🤐": "Zipper-Mouth Face",
+    "🤨": "Face with Raised Eyebrow",
+    "😐": "Neutral Face",
+    "😑": "Expressionless Face",
+    "😶": "Face Without Mouth",
+    "😏": "Smirking Face",
+    "😒": "Unamused Face",
+    "🙄": "Face with Rolling Eyes",
+    "😬": "Grimacing Face",
+    "😮‍💨": "Face Exhaling",
+    "🤥": "Lying Face",
+    "😌": "Relieved Face",
+    "😔": "Pensive Face",
+    "😪": "Sleepy Face",
+    "🤤": "Drooling Face",
+    "😴": "Sleeping Face",
+    "😷": "Face with Medical Mask",
+    "🤒": "Face with Thermometer",
+    "🤕": "Face with Head-Bandage",
+    "🤢": "Nauseated Face",
+    "🤮": "Face Vomiting",
+    "🤧": "Sneezing Face",
+    "🥵": "Hot Face",
+    "🥶": "Cold Face",
+    "🥴": "Woozy Face",
+    "😵": "Face with Crossed-Out Eyes",
+    "😵‍💫": "Face with Spiral Eyes",
+    "🤯": "Exploding Head",
+    "🤠": "Cowboy Hat Face",
+    "🥳": "Partying Face",
+    "🥸": "Disguised Face",
+    "😎": "Smiling Face with Sunglasses",
+    "🤓": "Nerd Face",
+    "🧐": "Face with Monocle",
+    "😕": "Confused Face",
+    "😟": "Worried Face",
+    "🙁": "Slightly Frowning Face",
+    "☹️": "Frowning Face",
+    "😮": "Face with Open Mouth",
+    "😯": "Hushed Face",
+    "😲": "Astonished Face",
+    "😳": "Flushed Face",
+    "🥺": "Pleading Face",
+    "😦": "Frowning Face with Open Mouth",
+    "😧": "Anguished Face",
+    "😨": "Fearful Face",
+    "😰": "Anxious Face with Sweat",
+    "😥": "Sad but Relieved Face",
+    "😢": "Crying Face",
+    "😭": "Loudly Crying Face",
+    "😱": "Face Screaming in Fear",
+    "😖": "Confounded Face",
+    "😣": "Persevering Face",
+    "😞": "Disappointed Face",
+    "😓": "Downcast Face with Sweat",
+    "😩": "Weary Face",
+    "😫": "Tired Face",
+    "🥱": "Yawning Face",
+    "😤": "Face with Steam From Nose",
+    "😡": "Enraged Face",
+    "😠": "Angry Face",
+    "🤬": "Face with Symbols on Mouth",
+    "😈": "Smiling Face with Horns",
+    "👿": "Angry Face with Horns",
     "🐕": "Dog",
     "🦮": "Guide Dog",
     "🐕‍🦺": "Service Dog",
@@ -49,11 +136,13 @@ const App = () => {
     "🦊": "Fox",
     "🦝": "Raccoon",
     "🐱": "Cat Face",
+    "🐭": "Mouse Face",
     "🐈": "Cat",
     "🐈‍⬛": "Black Cat",
     "🦁": "Lion",
     "🐯": "Tiger Face",
-    "🐅": "Tiger",  //43
+    "🐅": "Tiger",
+    "🐗": "Boar",
     "🍇": "Grapes",
     "🍈": "Melon",
     "🍉": "Watermelon",
@@ -85,11 +174,49 @@ const App = () => {
     "🥦": "Broccoli",
     "🧄": "Garlic",
     "🧅": "Onion",
-    "🍄": "Mushroom",
+    "🎂": "Birthday Cake",
+    "🍰": "Shortcake",
+    "🎈": "Balloon",
+    "🎉": "Party Popper",
+    "🎁": "Wrapped Gift",
+    "🕯️": "Candle",
     "🥜": "Peanuts",
+    "👋": "Waving Hand",
+    "🤚": "Raised Back of Hand",
+    "🖐️": "Hand with Fingers Splayed",
+    "✋": "Raised Hand",
+    "🖖": "Vulcan Salute",
+    "👌": "OK Hand",
+    "🤌": "Pinched Fingers",
+    "🤏": "Pinching Hand",
+    "✌️": "Victory Hand",
+    "🤞": "Crossed Fingers",
+    "🤟": "Love-You Gesture",
+    "🤘": "Sign of the Horns",
+    "🤙": "Call Me Hand",
+    "👈": "Backhand Index Pointing Left",
+    "👉": "Backhand Index Pointing Right",
+    "👆": "Backhand Index Pointing Up",
+    "🖕": "Middle Finger",
+    "👇": "Backhand Index Pointing Down",
+    "☝️": "Index Pointing Up",
+    "👍": "Thumbs Up",
+    "👎": "Thumbs Down",
+    "✊": "Raised Fist",
+    "👊": "Oncoming Fist",
+    "🤛": "Left-Facing Fist",
+    "🤜": "Right-Facing Fist",
+    "👏": "Clapping Hands",
+    "🙌": "Raising Hands",
+    "👐": "Open Hands",
+    "🤲": "Palms Up Together",
+    "🤝": "Handshake",
+    "🪲": "Beetle",
 
   };
   var length = Object.keys(emojiDictionary).length;
+
+  // console.log(length);
 
   const toggleMode = () => {
     if (!menu) {
@@ -103,17 +230,31 @@ const App = () => {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
-      if (len === 43 && btnName === "Show More") {
-        setLen(length)
-        setBtnName("Show less")
-      } else {
-        setLen(43)
+
+      if (btnName === "Show More" && len === 50) {
+        setLen(len + 50);
+        setBtnName("Show More")
+      }
+      if (btnName === "Show More" && len === 100) {
+        setLen(len + 50);
+        setBtnName("Show More")
+      }
+      if (btnName === "Show More" && len === 150) {
+        setLen(len + (length - len));
+        setBtnName("Show Less")
+
+      }
+     
+      if (btnName === "Show Less") {
+        setLen(50);
         setBtnName("Show More")
       }
 
-    }, 900);
-   
+    }, 800);
+
   }
+
+
   const [transform, settransform] = useState(false);
   const changeColor = () => {
     if (!transform) {
